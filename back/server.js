64527bf6ddcc7a4e34c3b45a61d1873626ebdee5
Paul Waligora <paul.waligora@hotmail.fr>
2021-09-03 15:57:40 +0200

    .env added and project finished

--- back/server.js
+++ back/server.js
@@ -7,7 +7,20 @@ require('dotenv').config({path:'./config/.env'})
 require('./config/db')
 
 const {checkUsers, requireAuth} = require('./middleware/auth.middleware')
+const cors = require('cors')
 const app = express(); // app est le framework express
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    Credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}
+
+//cors
+app.use(cors({corsOptions}))
 
 // middlewares
 app.use(express.json())
